5ddcf8f42d534c0d8763efc0822e5c1b167a91c8
Joseph Dixon <jdixon@horncadcenter.com>
2025-03-09 14:28:53 -0600

    Added images and text

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Set the current year dynamically
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Set the last modified date dynamically
+document.getElementById("lastModified").textContent =
+  "Last Modified: " + document.lastModified;
